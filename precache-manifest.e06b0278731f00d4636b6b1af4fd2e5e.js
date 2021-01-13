3deaf9d616fe9ee8aa6637996bb1b7d25b552dc3
omarandres2803 <omarandres2803@gmail.com>
2021-01-13 21:53:15 +0100

    Updates

---
+++ precache-manifest.e06b0278731f00d4636b6b1af4fd2e5e.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f947671fae33b71c6a8781673511e7b",
+    "url": "/hostNikeWebpageReact/index.html"
+  },
+  {
+    "revision": "2b6469f57de1cfa380c2",
+    "url": "/hostNikeWebpageReact/static/css/main.d62e4dc1.chunk.css"
+  },
+  {
+    "revision": "bd950fa47a576ffc1213",
+    "url": "/hostNikeWebpageReact/static/js/2.3b19acd0.chunk.js"
+  },
+  {
+    "revision": "2b6469f57de1cfa380c2",
+    "url": "/hostNikeWebpageReact/static/js/main.60657ed4.chunk.js"
+  },
+  {
+    "revision": "65906e07b77de82f7e5e",
+    "url": "/hostNikeWebpageReact/static/js/runtime~main.eee8429b.js"
+  },
+  {
+    "revision": "5d37a4112109486acb12a715df97e3a9",
+    "url": "/hostNikeWebpageReact/static/media/blackBoots.5d37a411.webp"
+  },
+  {
+    "revision": "01b8faf56997c14871074330a4649605",
+    "url": "/hostNikeWebpageReact/static/media/blueBoots.01b8faf5.webp"
+  },
+  {
+    "revision": "08579e88ff5b0f4ae23ea4887c2cd7e8",
+    "url": "/hostNikeWebpageReact/static/media/cardio.08579e88.jpg"
+  },
+  {
+    "revision": "65b3e54930e854e199d6f3c387f62682",
+    "url": "/hostNikeWebpageReact/static/media/featured1.65b3e549.jpg"
+  },
+  {
+    "revision": "eb6ebf96e136ede092246ab95b9319c8",
+    "url": "/hostNikeWebpageReact/static/media/featured2.eb6ebf96.jpg"
+  },
+  {
+    "revision": "7276c2bdfc8fd6ae9f6f4957620b78da",
+    "url": "/hostNikeWebpageReact/static/media/groupWomen.7276c2bd.jpg"
+  },
+  {
+    "revision": "462527356c73ae5897582d2fdb2c8847",
+    "url": "/hostNikeWebpageReact/static/media/gym.46252735.jpg"
+  },
+  {
+    "revision": "ee3749dc07af76dd1f985aba8268ced5",
+    "url": "/hostNikeWebpageReact/static/media/highIntensity.ee3749dc.jpg"
+  },
+  {
+    "revision": "4498fb722e58f2009c4ed49a713c3b6a",
+    "url": "/hostNikeWebpageReact/static/media/hombres.4498fb72.jpg"
+  },
+  {
+    "revision": "bd2d51172e4d053928a1a79c8b5fbadc",
+    "url": "/hostNikeWebpageReact/static/media/justDoIt.bd2d5117.jpg"
+  },
+  {
+    "revision": "c31e51670eafd8e78d873359c25269dd",
+    "url": "/hostNikeWebpageReact/static/media/nikenba.c31e5167.jpg"
+  },
+  {
+    "revision": "207cbc3212f1fcdd8fbf278c3e8ebc52",
+    "url": "/hostNikeWebpageReact/static/media/redBoots.207cbc32.jpg"
+  },
+  {
+    "revision": "635fcc338e40600d27663c3cfc888ffc",
+    "url": "/hostNikeWebpageReact/static/media/studioClass.635fcc33.jpg"
+  },
+  {
+    "revision": "0fb67354a6260d678bcc09a735da11db",
+    "url": "/hostNikeWebpageReact/static/media/whiteBoots.0fb67354.webp"
+  },
+  {
+    "revision": "86ff1ed94b5f510628394143b6a232f2",
+    "url": "/hostNikeWebpageReact/static/media/womenTraining.86ff1ed9.jpg"
+  }
+]);
